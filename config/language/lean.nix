0a1ff116f0e9768302822a9bb7212f059aa60f13
Moritz Noebauer <moritz.noebauer@gmail.com>
2025-11-14 14:35:09 +0100

    add loogle telescope keybind

--- config/language/lean.nix
+++ config/language/lean.nix
@@ -1,3 +1,16 @@
 {
   plugins.lean.enable = true;
+
+  # TODO: Can this be lazy loaded when lean & telescope is in scope?
+  files."ftplugin/lean.lua".keymaps = [
+    {
+      action = "<cmd>Telescope loogle<CR>";
+      key = "<leader>sl";
+      options = {
+        unique = true;
+        desc = "[S]earch [L]oogle";
+      };
+    }
+  ];
+
 }
