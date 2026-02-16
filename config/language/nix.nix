dc29bfb5c7bdfbb9389327680bcd2e1f7a20ed0b
Moritz Noebauer <moritz.noebauer@gmail.com>
2026-02-16 12:08:46 +0100

    nix: use nixfmt package instead (as per eval warning)

--- config/language/nix.nix
+++ config/language/nix.nix
@@ -1,16 +1,16 @@
-{pkgs, ...}: {
+{ pkgs, ... }:
+{
   # filetype detection, indentation, syntax highlighting
   plugins.nix.enable = true;
 
   plugins.lsp.servers.nil_ls = {
     enable = true;
     settings = {
-      formatting.command = ["nixfmt"];
+      formatting.command = [ "nixfmt" ];
       # get completion info from flake inputs?
       flake.autoEvalInputs = true;
     };
   };
 
-  # use `nixfmt` for formatting
-  extraPackages = [pkgs.nixfmt-rfc-style];
+  extraPackages = [ pkgs.nixfmt ];
 }
